41c334b2d45e72bb596de968a0882558472c1e2d
achrefmechergui <achreffcb123achref@gamil.com>
2025-02-28 21:54:19 +0100

    display-userName-login-welcome-message

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2,
   pin: 1111,
+  
 };
 
 const account2 = {
@@ -12,6 +13,7 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, 1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  
 };
 
 const account3 = {
@@ -19,6 +21,7 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460, 100, -400],
   interestRate: 0.7,
   pin: 3333,
+ 
 };
 
 const account4 = {
@@ -26,17 +29,26 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+
 };
+
 
 const accounts = [account1, account2, account3, account4];
 
+console.log(accounts)
 
-////// import element :
+
+////// import elements :
 const movementContainer = document.querySelector(".left");
 const balanceAmount = document.querySelector(".amount");
 const inComes = document.querySelector(".mov-average-in")
 const outMoney = document.querySelector(".mov-average-out")
 const intrest = document.querySelector(".mov-average-interest")
+const btnLogin = document.querySelector(".auth-btn")
+const userNameInput = document.querySelector(".userName")
+const pinInput = document.querySelector(".pin")
+const welcomeMessage = document.querySelector(".welcome")
+
 
 
 
@@ -109,6 +121,39 @@ const calcDisplaySummary = function (accountMovement){
 
 
 calcDisplaySummary(account2.movements) ; 
+
+
+//////// userName key functionnality ::: ////// 
+
+const displayUserName = function (arr){
+  arr.forEach((person)=>{
+   person.userName = person.owner.toLowerCase().split(" ")
+   .map((ele)=> ele[0])  //// ['jessica', 'davis']   //// [j,d]
+   .join("") //jd
+  })
+}
+
+displayUserName(accounts)
+
+
+////// display login :: ///////////////////// 
+let currentAccount ; 
+btnLogin.addEventListener("click",function(){
+ currentAccount = accounts.find((ele)=>{
+  return ele.userName === userNameInput.value //ss
+ })
+console.log(currentAccount)
+
+if (currentAccount.pin === Number(pinInput.value)){
+  welcomeMessage.textContent = `welcome back ${currentAccount.owner.split(" ")[0]}`
+}
+
+userNameInput.value = pinInput.value =  "" 
+
+
+
+})
+
 
 
 
